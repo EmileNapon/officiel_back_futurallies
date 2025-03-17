b0a72dc6ab33a0690d64112126ba430927b682f1
EmileNapon <emilenapon0@gmail.com>
2025-03-17 22:29:36 +0000

    first commitmm

--- futallies-backend/users/url.py
+++ futallies-backend/users/url.py
@@ -4,7 +4,9 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import (
     CustomTokenObtainPairView, RegisterView, UserDetailView, 
-    list_users, UpdateUserView, DeleteUserView
+    list_users, UpdateUserView, DeleteUserView, ApprenantListView, ApprenantDetailView, EncadrantListView, EncadrantDetailView,
+
+    EntreprisesListView, EntrepriseDetailView
 )
 
 urlpatterns = [
@@ -16,8 +18,20 @@ urlpatterns = [
     # Gestion des utilisateurs
     path('fidalli/users/', list_users, name='list_users'),  # Liste des utilisateurs
     path('fidalli/users/me/', UserDetailView.as_view(), name='user-detail'),  # Infos utilisateur connecté
-    path('fidalli/users/update/', UpdateUserView.as_view(), name='update-user'),  # Mise à jour de l'utilisateur connecté
-    path('fidalli/users/me/delete/', DeleteUserView.as_view(), name='delete-user'),  # Suppression du compte utilisateur
+    path('fidalli/users/<int:pk>/', UpdateUserView.as_view(), name='update-user'),  # Mise à jour de l'utilisateur connecté
+    path('fidalli/users/delete/<int:pk>/', DeleteUserView.as_view(), name='delete-user'),  # Suppression du compte utilisateur
+
+
+    #filtre
+
+     path('fidalli/apprenants/', ApprenantListView.as_view(), name='etudiant-list'),
+     path('fidalli/apprenants/<int:id>/', ApprenantDetailView.as_view(), name='apprenant-detail'),
+
+     path('fidalli/encadrants/', EncadrantListView.as_view(), name='encadrants-list'),
+     path('fidalli/encadrants/<int:id>/', EncadrantDetailView.as_view(), name='encadrants-detail'),
+
+      path('fidalli/entreprises/', EntreprisesListView.as_view(), name='employeurs-list'),
+     path('fidalli/entreprises/<int:id>/', EntrepriseDetailView.as_view(), name='employeurs-detail'),
 ]
 
 # Ajout du support pour servir les fichiers médias en mode DEBUG
--- futallies-backend/users/views.py
+++ futallies-backend/users/views.py
@@ -1,4 +1,6 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics, status
+from rest_framework.parsers import JSONParser, MultiPartParser, FormParser
 from .models import CustomUser
 from .serializers import (
     CustomTokenObtainPairSerializer, 
@@ -27,14 +29,17 @@ class UserDetailView(generics.RetrieveAPIView):
     def get_object(self):
         return self.request.user  # Récupère l'utilisateur connecté au lieu d'un autre utilisateur
 
-# Vue pour la mise à jour d'un utilisateur
+# Vue pour la mise à jour d'un utilisateurfrom rest_framework.parsers import JSONParser, MultiPartParser, FormParser
+
 class UpdateUserView(generics.UpdateAPIView):
     serializer_class = UpdateUserSerializer
     permission_classes = [IsAuthenticated]
-    parser_classes = (MultiPartParser, FormParser)  # Accepte l'upload d'images
-
+    parser_classes = [JSONParser, MultiPartParser, FormParser]  # ✅ Ajout de JSONParser
     def get_object(self):
-        return self.request.user  # Un utilisateur ne peut modifier que son propre compte
+        user_id = self.kwargs.get("pk")
+        return get_object_or_404(CustomUser, pk=user_id)
+
+
 
 # Vue pour la suppression d'un utilisateur
 class DeleteUserView(generics.DestroyAPIView):
@@ -54,3 +59,47 @@ def list_users(request):
     users = CustomUser.objects.all().order_by('-id')  # Trie les utilisateurs par ID décroissant
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ApprenantListView(generics.ListAPIView):
+    queryset = CustomUser.objects.filter(role='apprenant')  
+    serializer_class = UserSerializer
+
+
+class ApprenantDetailView(generics.RetrieveAPIView):
+    queryset = CustomUser.objects.filter(role='apprenant')
+    serializer_class = UserSerializer
+    lookup_field = 'id'  
+
+
+class DeleteUserView(generics.DestroyAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = [IsAuthenticated]  
+    def get_object(self):
+        user_id = self.kwargs.get("pk")
+        return get_object_or_404(CustomUser, pk=user_id)
+    
+
+
+
+class EncadrantListView(generics.ListAPIView):
+    queryset = CustomUser.objects.filter(role='formateur')  
+    serializer_class = UserSerializer
+
+
+class EncadrantDetailView(generics.RetrieveAPIView):
+    queryset = CustomUser.objects.filter(role='formateur')
+    serializer_class = UserSerializer
+    lookup_field = 'id'  
+
+
+
+class EntreprisesListView(generics.ListAPIView):
+    queryset = CustomUser.objects.filter(role='employeur')  
+    serializer_class = UserSerializer
+
+
+class EntrepriseDetailView(generics.RetrieveAPIView):
+    queryset = CustomUser.objects.filter(role='employeur')
+    serializer_class = UserSerializer
+    lookup_field = 'id'  
